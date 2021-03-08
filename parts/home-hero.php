99432a5a8f453d45027bba3fd6b0b85bd5d63db0
Ben Kaminski <ben@benkaminski.com>
2021-03-08 17:16:40 -0500

    added webfont

--- parts/home-hero.php
+++ parts/home-hero.php
@@ -1,7 +1,7 @@
 <header>
 	<div class="home-hero">
 		<div class="container">
-			<h1 class="center-hv" style="min-height:500px;">Booth House Tavern</h1>
+			<h1 class="center-hv bh-font display-1" style="min-height:500px;">Booth House Tavern</h1>
 		</div>
 	</div>
 </header>
