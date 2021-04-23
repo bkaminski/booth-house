ac5cdb01e778e3c4b6f3cb4a14b6f098f7016f95
Ben Kaminski <ben@benkaminski.com>
2021-04-23 19:06:33 -0400

    added waypoints

--- parts/home-hero.php
+++ parts/home-hero.php
@@ -1,6 +1,6 @@
 <header>
 	<div class="home-hero pt-2">
-		<div class="container text-center pt-5">
+		<div class="container text-center pt-5 hero--img">
 			<img class="img-fluid pt-5" src="<?php echo get_home_url(); ?>/wp-content/uploads/bh-hero-logo.png" width="480" />
 		</div>
 	</div>
